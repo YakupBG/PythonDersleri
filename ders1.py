52fa0e95ad820539193312c39b9cc6dbb16e164e
YakupBG <54790006+YakupBG@users.noreply.github.com>
2021-02-23 20:09:25 +0300

    ilk projem

---
+++ ders1.py
@@ -0,0 +1,21 @@
+import turtle as kalem
+
+kalem.pencolor( 'blue' )
+kalem.pensize(2)
+
+
+kalem.forward(100)  # ileri 100 adım git
+kalem.left(90)
+kalem.fd(100)
+kalem.left(90)
+kalem.fd(100) # fd = forward kısaltması
+kalem.lt(90)  # lt = left kısaltması
+kalem.fd(100)
+
+input()
+ 
+
+
+
+
+ 
